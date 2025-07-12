9d429552d1c6622644ec3002740e1c95ccee0baf
Igor Davydov <maink@list.ru>
2025-07-12 12:01:05 +0300

    fix

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -4,8 +4,10 @@ import requests
 
 BASE_URL = "https://postman-echo.com"
 
+# get запрос
 def test_get():
     response = requests.get(f"{BASE_URL}/get")
     assert response.status_code == 200
     assert "args" in response.json()
     assert response.json()["url"] == f"{BASE_URL}/get"
+
